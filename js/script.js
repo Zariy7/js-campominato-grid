f5cabca8c65f11c4d6d8c730b1b48bb1f9c0befe
Zariy7 <nazariy.hereha@gmail.com>
2023-11-30 06:46:36 +0100

    variable size grid generation set up

--- js/script.js
+++ js/script.js
@@ -1,7 +1,42 @@
 let genBtn = document.getElementById('Generate');
 
-genBtn.addEventListener('click', GridGen);
+genBtn.addEventListener('click', gridGen);
 
-function GridGen(){
-    
+function gridGen(){
+    let diffString = document.getElementById('Difficulty').value;
+    let diffValue;
+
+    switch (diffString){
+        case 'Easy':
+            diffValue = 10;
+            break;
+        case 'Medium':
+            diffValue = 9;
+            break;
+        case 'Hard':
+            diffValue = 7;
+            break;
+        default:
+            break;
+    }
+
+    let grid = document.getElementById('Grid');
+    grid.innerHTML = ``;
+    let z = 0;
+
+    //ROW CREATION
+    for(let i=0; i<diffValue; i++){
+        let newRow = document.createElement('div');
+        newRow.classList.add("d-flex", "align-items-center", "justify-content-center");
+        grid.appendChild(newRow);
+
+        //COLUMN CREATION
+        for(let j=0; j<diffValue; j++){
+            let newCol = document.createElement('div');
+            newCol.classList.add("custom-col", "d-flex", "align-items-center", "justify-content-center", "mx-1", "my-1");
+            newCol.textContent = (z+1);
+            z++;
+            newRow.appendChild(newCol);
+        }
+    }
 }
